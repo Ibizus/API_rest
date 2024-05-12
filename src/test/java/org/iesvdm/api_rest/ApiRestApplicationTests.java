a480824ea78de7cc7e0c0efc518da938d6e84677
Ibizus <hector.ldz@gmail.com>
2024-05-12 22:17:53 +0100

    Test ampliados

--- src/test/java/org/iesvdm/api_rest/ApiRestApplicationTests.java
+++ src/test/java/org/iesvdm/api_rest/ApiRestApplicationTests.java
@@ -1,9 +1,11 @@
 package org.iesvdm.api_rest;
 
 import org.iesvdm.api_rest.domain.Boda;
+import org.iesvdm.api_rest.domain.Evento;
 import org.iesvdm.api_rest.domain.Menu;
 import org.iesvdm.api_rest.domain.Usuario;
 import org.iesvdm.api_rest.repository.BodaRepository;
+import org.iesvdm.api_rest.repository.EventoRepository;
 import org.iesvdm.api_rest.repository.MenuRepository;
 import org.iesvdm.api_rest.repository.UsuarioRepository;
 import org.junit.jupiter.api.*;
@@ -24,6 +26,8 @@ class ApiRestApplicationTests {
     BodaRepository bodaRepository;
     @Autowired
     MenuRepository menuRepository;
+    @Autowired
+    EventoRepository eventoRepository;
 
     Usuario admin;
     Usuario user;
@@ -33,6 +37,11 @@ class ApiRestApplicationTests {
     Menu menuVegano;
     Menu menuCarne;
     Menu menuPescado;
+    Evento ceremonia;
+    Evento cocktail;
+    Evento banquete;
+    Evento fiesta;
+
 
     @BeforeEach
     void inicializa(){
@@ -48,7 +57,6 @@ class ApiRestApplicationTests {
     @Test
     @Order(2)
     void crearUsuarios(){
-
         admin = new Usuario(0, "Hector", "Lopez", "Diaz", "calle Veleta 2", "", "29651", "Mijas", "Malaga", "hlopdia699@g.educaand.es","665661519", new HashSet<>());
         user = new Usuario(0, "Alvaro", "Moreno", "Barreiro", "calle de la Luz 25", "", "29640", "Fuengirola", "Malaga", "alvaro@educaand.es","666666666", new HashSet<>());
         usuarioRepository.save(admin);
@@ -56,17 +64,20 @@ class ApiRestApplicationTests {
     }
 
     @Test
-    @Order(2)
-    void crearBodas() {
-
-//        boda1 = new Boda(0, "Mi Boda", LocalDate.now(), LocalTime.now(), "Alvaro", "Loli", "Finca Las Yeguas", "", "29600", "Yecla", "Malaga", new HashSet<>(), user);
-//        boda2 = new Boda(0, "Boda Cuñis", LocalDate.now(), LocalTime.now(), "Paco", "Caro", "Finca El Agua", "", "29720", "Marbella", "Malaga", new HashSet<>(), user);
-//        bodaRepository.save(boda1);
-//        bodaRepository.save(boda2);
+    @Order(3)
+    void crearEventos() {
+        ceremonia = new Evento(0, "Iglesia Niño Jesús", "Celebración del matrimonio", LocalTime.of(18, 0), boda1);
+        cocktail = new Evento(0, "Jardín Finca Las Yeguas", "Bienvenida a la finca", LocalTime.of(19, 30), boda1);
+        banquete = new Evento(0, "Salón interior", "Banquete de bodas", LocalTime.of(21, 0), boda1);
+        fiesta = new Evento(0, "Discoteca", "Espectáculo y barra libre para todos", LocalTime.of(23, 30), boda1);
+        eventoRepository.save(ceremonia);
+        eventoRepository.save(cocktail);
+        eventoRepository.save(banquete);
+        eventoRepository.save(fiesta);
     }
 
     @Test
-    @Order(3)
+    @Order(4)
     void crearMenus() {
         menuInfantil = new Menu(0, "Infantil", "Macarrones con queso", "Salchichas", "Yogur", boda1);
         menuVegano = new Menu(0, "Vegano", "Ensalada", "Lasaña vegana", "Pastel de manzana vegano", boda1);
@@ -79,10 +90,21 @@ class ApiRestApplicationTests {
     }
 
     @Test
-    @Order(4)
+    @Order(5)
+    void crearBodas() {
+
+        boda1 = new Boda(0, "Mi Boda", LocalDate.now(), LocalTime.now(), "Alvaro", "Loli", "Finca Las Yeguas", "", "29600", "Yecla", "Malaga", new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), user);
+        boda2 = new Boda(0, "Boda Cuñis", LocalDate.now(), LocalTime.now(), "Paco", "Caro", "Finca El Agua", "", "29720", "Marbella", "Malaga", new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), user);
+        bodaRepository.save(boda1);
+        bodaRepository.save(boda2);
+    }
+
+    @Test
+    @Order(6)
     void asociaciones() {
         // AÑADE MENUS A BODAS:
-        boda1.getMenus().forEach(System.out::println);
+        user.getBodas().add(boda1);
         user.getBodas().forEach(System.out::println);
+        boda1.getMenus().forEach(System.out::println);
     }
 }
