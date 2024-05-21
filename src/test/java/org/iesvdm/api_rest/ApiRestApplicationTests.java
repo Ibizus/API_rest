8180c9816b6cf2b1932e7fb572462e268a2e9d46
Ibizus <hector.ldz@gmail.com>
2024-05-21 23:23:41 +0100

    Data creation finished

--- src/test/java/org/iesvdm/api_rest/ApiRestApplicationTests.java
+++ src/test/java/org/iesvdm/api_rest/ApiRestApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.Normalizer;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.HashSet;
@@ -102,30 +103,57 @@ class ApiRestApplicationTests {
     @Test
     @Order(6)
     void crearInvitaciones() {
-        Invitacion invitacion1 = new Invitacion(0, "Antonio", "antofenomeno@example.com", false, "", boda1, null);
-        invitacionRepository.save(invitacion1);
+
+        String[] listaInvitaciones = {
+                "Francisco", "Antonio", "José", "Manuel", "María", "Ana", "Carmen", "Elena", "Isabel",
+                "Laura", "David", "Javier", "Daniel", "Sofía", "Luis", "Miguel", "Rosa", "Carlos",
+                "Pedro", "Raúl", "Andrea", "Patricia", "Lucía", "Diego", "Ángela", "Natalia", "Roberto",
+                "Cristina", "Jorge", "Beatriz", "Fernando", "Silvia", "Pablo", "Verónica", "Rubén",
+                "María José", "Adrián", "Eva", "José Antonio", "María Teresa", "Rafael", "Celia",
+                "Alejandro", "María Isabel", "Ignacio", "Carmen María", "Alberto", "María Luisa",
+                "Víctor", "Ana María", "Óscar", "María Pilar", "Guillermo", "María Dolores", "Mario",
+                "María Ángeles", "Irene", "María Victoria", "Hugo", "María Elena", "Javier", "María Carmen",
+                "Izan", "María Consuelo", "Aitana", "María Emilia", "Alba", "María Milagros", "Olivia",
+                "María Candelaria", "Emma", "María Esperanza", "Valentina", "María África", "Luna",
+                "María Gracia", "Ainhoa", "María Lourdes", "Cayetana", "María del Mar", "Leire",
+                "María del Carmen", "Nora", "María del Pilar", "Alonso", "María de los Ángeles", "Mateo",
+                "Leo", "Eric", "Izan", "Ariadna", "Noa", "Enzo", "Marco", "Ian"
+        };
+
+        for (String nombreInvitado : listaInvitaciones) {
+            String nombreCorreo = Normalizer.normalize(nombreInvitado, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").replaceAll("\\s+", "").toLowerCase();
+            Invitacion invitacion = new Invitacion(0, nombreInvitado, nombreCorreo+"@email.com", false, "", boda1, null);
+            invitacionRepository.save(invitacion);
+        }
     }
 
     @Test
     @Order(7)
     void crearRegalos() {
-        Regalo regalo1 = new Regalo(0, "Viaje", false, boda1);
-        Regalo regalo2 = new Regalo(0, "Lavadora", false, boda1);
-        Regalo regalo3 = new Regalo(0, "Carrito bebe", false, boda1);
-        Regalo regalo4 = new Regalo(0, "Television", false, boda1);
-        Regalo regalo5 = new Regalo(0, "Bicicleta", false, boda1);
-        Regalo regalo6 = new Regalo(0, "Suscripcion", false, boda1);
-        Regalo regalo7 = new Regalo(0, "Cesta productos artesanos", false, boda1);
-        Regalo regalo8 = new Regalo(0, "Reloj", false, boda1);
-        Regalo regalo9 = new Regalo(0, "Zapatos", false, boda1);
-        Regalo regalo10 = new Regalo(0, "Viaje", false, boda1);
-        Regalo regalo11 = new Regalo(0, "Viaje", false, boda1);
-        Regalo regalo12 = new Regalo(0, "Viaje", false, boda1);
-        Regalo regalo13 = new Regalo(0, "Viaje", false, boda1);
-        Regalo regalo14 = new Regalo(0, "Viaje", false, boda1);
-        Regalo regalo15 = new Regalo(0, "Viaje", false, boda1);
 
-        regaloRepository.save(regalo1);
+        String[] listaRegalos = {
+                "Viaje", "Lavadora", "Carrito bebe", "Television", "Bicicleta",
+                "Suscripcion Netflix", "Cesta productos artesanos", "Juego de sábanas",
+                "Robot de cocina", "Freidora de aire", "Altavoz bluetooth", "Juegos de cuchillos",
+                "Portarretratos", "Copas", "Marcos para fotos", "Obras de arte", "Cojines decorativos",
+                "Lámparas de mesa", "Relojes de pared", "Cafetera", "Robot de cocina", "Licuadora y picadora",
+                "Robot de limpieza", "Minibar", "Plancha de ropa", "Procesador de alimentos",
+                "Cuchillos y tablas de cortar", "Jarras, licoreras, posavasos", "Sartenes y ollas",
+                "Coladores y refractarios", "Azucareros, saleros y pimenteros", "Aceitera y vinagrera",
+                "Juego de sábanas", "Robot aspirador", "Set de copas de vino", "Caja de herramientas",
+                "Set de toallas", "Licuadora de mano", "Set de especias", "Tostadora", "Batería de cocina",
+                "Caja de vinos", "Set de velas aromáticas", "Tabla de quesos", "Licor premium", "Set de tazas de café",
+                "Caja de bombones", "Set de organizadores", "Set de cubiertos", "Caja de té o café", "Set de vasos",
+                "Caja de aceites gourmet", "Set de manteles", "Set de copas de champagne", "Caja de productos gourmet",
+                "Set de posavasos", "Set de platos", "Caja de vinos personalizada", "Set de servilletas",
+                "Set de copas de cóctel", "Caja de chocolates"
+        };
+
+        for (String nombreRegalo : listaRegalos) {
+            Regalo regalo = new Regalo(0, nombreRegalo, false, boda1);
+            regaloRepository.save(regalo);
+        }
+
     }
 
     @Test
@@ -136,10 +164,30 @@ class ApiRestApplicationTests {
     }
 
     @Test
+    @Order(9)
+    void crearInvitados() {
+
+        String[] listaApellidoss = {
+               "García", "Rodríguez", "González", "Fernández", "López", "Martínez", "Sánchez", "Pérez",
+                "Gómez", "Jiménez", "Hernández", "Ruiz", "Díaz", "Moreno", "Muñoz", "Álvarez", "Romero",
+                "Gutiérrez", "Alonso", "Navarro", "Torres", "Domínguez", "Ramos", "Vázquez", "Ramírez",
+                "Gil", "Serrano", "Morales", "Molina", "Blanco", "Suárez", "Castro", "Ortega", "Delgado",
+                "Ortiz", "Marín", "Rubio", "Núñez", "Medina", "Sanz", "Castillo", "Iglesias", "Cortés",
+                "Garrido", "Santos", "Guerrero", "Lozano", "Cano", "Cruz", "Méndez", "Flores", "Prieto",
+                "Herrera", "Peña", "León", "Márquez", "Cabrera", "Gallego", "Calvo", "Vidal", "Campos",
+                "Reyes", "Vega", "Fuentes", "Carrasco", "Diez", "Aguilar", "Caballero", "Nieto", "Santana",
+                "Vargas", "Pascual", "Giménez", "Herrero", "Hidalgo", "Montero", "Lorenzo", "Santiago",
+                "Benítez", "Durán", "Ibáñez", "Arias", "Mora", "Ferrer", "Carmona", "Vicente", "Rojas",
+                "Soto", "Crespo", "Román", "Pastor", "Velasco", "Parra", "Sáez", "Moya", "Bravo", "Rivera",
+                "Gallardo"
+        };
+    }
+
+    @Test
     @Order(99)
     void asociaciones() {
         // AÑADE MENUS A BODAS:
-        user.getBodas().add(boda1);
+        //user.getBodas().add(boda1);
         System.out.println("Bodas del user 1: ");
         user.getBodas().forEach(System.out::println);
         System.out.println("Menus de la boda: ");
@@ -148,6 +196,8 @@ class ApiRestApplicationTests {
         boda1.getTareas().forEach(System.out::println);
         System.out.println("Regalos boda: ");
         boda1.getRegalos().forEach(System.out::println);
+        System.out.println("Invitaciones boda: ");
+        boda1.getInvitaciones().forEach(System.out::println);
     }
 
 
